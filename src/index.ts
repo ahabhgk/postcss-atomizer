93000d121cb705d2f5be1c0b9882938b3ab3640c
ahabhgk <ahabhgk@gmail.com>
2021-09-10 16:58:46 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import { PluginCreator } from 'postcss'
+import { Declaration, PluginCreator, Rule } from 'postcss'
 import { resolve } from 'path'
 import { writeFile } from 'fs/promises'
 
 interface Options {
   atomicCssFile: string;
+}
+
+interface DeclCounter {
+  first: Declaration;
+  decl: Declaration;
+  count: number;
+  cls: string;
 }
 
 function hash(s: string): string {
@@ -17,38 +24,79 @@ function hash(s: string): string {
   return hash.toString();
 };
 
+function clsKey(decl: Declaration): string {
+  return `${decl.prop}+${decl.value.replace(/ /g, '_')}`
+}
+
+function hashify(key: string): string {
+  return `a${hash(key)}`
+}
+
 const creator: PluginCreator<Options> = (opts = { atomicCssFile: resolve(process.cwd(), 'dist', 'atomic.css') }) => {
+  const atomicMap = new Map<string, DeclCounter>()
   const { atomicCssFile } = opts
-  const atomicMap = new Map()
+
+  function countCommonDecl(decl: Declaration): DeclCounter {
+    const key = clsKey(decl)
+    const value = atomicMap.get(key)
+    if (value === undefined) {
+      const cls = hashify(key)
+      const counter = { first: decl, decl: decl.clone(), count: 1, cls }
+      atomicMap.set(key, counter)
+      return counter
+    }
+    const counter = value
+    counter.count += 1
+    atomicMap.set(key, counter)
+    return counter
+  }
 
   return {
     postcssPlugin: 'postcss-atomizer',
 
-    Declaration(decl, postcss) {
+    Declaration(decl, { Rule }) {
       if (decl.prop === 'composes') return
+      if (decl.parent?.type === 'rule' && !(decl.parent as Rule).selector.startsWith('.')) return
 
-      const cls = `a${hash(`${decl.prop}+${decl.value.replace(/ /g, '_')}`)}`
-      const old = decl.replaceWith({ prop: 'composes', value: `${cls} from "${atomicCssFile}"` })
-      if (!atomicMap.has(cls)) {
-        atomicMap.set(cls, old)
+      const { first, count, cls, decl: commonDecl } = countCommonDecl(decl)
+      if (count > 2) {
+        decl.replaceWith({ prop: 'composes', value: `${cls} from global` })
+      } else if (count === 1) {
+        // do nothing
+      } else {
+        const value = `${cls} from global`
+        const root = decl.root()
+        decl.replaceWith({ prop: 'composes', value })
+        const old = first.replaceWith({ prop: 'composes', value })
+        const rule = new Rule({ selector: `:global(.${cls})` })
+        rule.append(commonDecl)
+        root.append(rule)
+        console.log('first>>>>', first.toString(), old.toString())
+        console.log('root??????', root.toString())
       }
     },
 
     async OnceExit(root, { Root, result, Rule }) {
-      const atomicRoot = new Root()
-      const rules = [...atomicMap.entries()].map(([cls, decl]) => {
-        const rule = new Rule({ selector: `.${cls}` })
-        rule.append(decl)
-        return rule
-      })
-      atomicRoot.append(...rules)
-      result.messages.push({
-        type: 'dependency',
-        file: atomicCssFile,
-      })
-      console.log(atomicCssFile)
-      await writeFile(atomicCssFile, atomicRoot.toString())
-    },
+      console.log(root.toString())
+      console.log([...atomicMap.entries()].filter(([, { count }]) => count > 1).map(([key]) => key))
+    }
+    // async OnceExit(root, { Root, result, Rule }) {
+    //   const atomicRoot = new Root()
+    //   const rules = [...atomicMap.entries()]
+    //     .filter(([, { count }]) => count > 1)
+    //     .map(([, { decl, cls }]) => {
+    //       const rule = new Rule({ selector: `.${cls}` })
+    //       rule.append(decl)
+    //       return rule
+    //     })
+    //   atomicRoot.append(...rules)
+    //   result.messages.push({
+    //     type: 'dependency',
+    //     file: atomicCssFile,
+    //   })
+    //   console.log('common: ', atomicRoot.toString())
+    //   await writeFile(atomicCssFile, atomicRoot.toString())
+    // },
   }
 }
 creator.postcss = true
